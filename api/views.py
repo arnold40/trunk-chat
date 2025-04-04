a33a6adcab5ed660834582b75517037453fb5a81
arnoldswang <arnold0121@gmail.com>
2025-04-04 11:08:56 +0200

    feat: set env file

--- api/views.py
+++ api/views.py
@@ -3,4 +3,4 @@ from rest_framework.response import Response
 
 @api_view(['GET'])
 def vegetarian_users(request):
-    return Response({"status": "ok", "message": "ALL VEGES ARE REALLY FOR YOU!!!"})
+    return Response({"status": "ok", "message": "ALL VEGES ARE REALLY FOR YOU!!"})
